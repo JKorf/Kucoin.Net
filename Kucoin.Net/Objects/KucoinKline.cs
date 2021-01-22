89a09291cf2ef4bfab13b60d7ee53ef834e63b21
Jkorf <jankorf91@gmail.com>
2021-01-22 15:14:38 +0100

    Updated ICommonKline

--- Kucoin.Net/Objects/KucoinKline.cs
+++ Kucoin.Net/Objects/KucoinKline.cs
@@ -51,5 +51,7 @@ namespace Kucoin.Net.Objects
         decimal ICommonKline.CommonLow => Low;
         decimal ICommonKline.CommonOpen => Open;
         decimal ICommonKline.CommonClose => Close;
+        decimal ICommonKline.CommonVolume => Volume;
+        DateTime ICommonKline.CommonOpenTime => StartTime;
     }
 }
