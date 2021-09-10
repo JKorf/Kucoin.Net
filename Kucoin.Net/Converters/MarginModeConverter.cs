a687cead46ad0ef1cb3bd71cd84dd8b21fbdbf38
Hammad Sajid <786hammadsajid@gmail.com>
2021-09-11 01:20:20 +0500

    PlaceMarginOrder method added

---
+++ Kucoin.Net/Converters/MarginModeConverter.cs
@@ -0,0 +1,19 @@
+﻿using CryptoExchange.Net.Converters;
+using Kucoin.Net.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kucoin.Net.Converters
+{
+    internal class MarginModeConverter : BaseConverter<KucoinMarginMode>
+    {
+        public MarginModeConverter() : this(true) { }
+        public MarginModeConverter(bool quotes) : base(quotes) { }
+        protected override List<KeyValuePair<KucoinMarginMode, string>> Mapping => new List<KeyValuePair<KucoinMarginMode, string>>
+        {
+            new KeyValuePair<KucoinMarginMode, string>(KucoinMarginMode.CrossMode, "cross"),
+            new KeyValuePair<KucoinMarginMode, string>(KucoinMarginMode.IsolatedMode, "isolated"),
+        };
+    }
+}
--- Kucoin.Net/Objects/KucoinEnums.cs
+++ Kucoin.Net/Objects/KucoinEnums.cs
@@ -525,4 +525,20 @@
         /// </summary>
         MarginTrade
     }
+
+    /// <summary>
+    /// Mode of Margin
+    /// </summary>
+    public enum KucoinMarginMode
+    {
+        /// <summary>
+        /// Cross Mode
+        /// </summary>
+        CrossMode,
+        /// <summary>
+        /// Isolated Mode, This mode is not supported by platform yet.
+        /// </summary>
+        IsolatedMode,
+    }
+
 }
