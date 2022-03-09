08cb825de5a93ad8c5033cca4acbb8e44f2f99a0
Jkorf <jankorf91@gmail.com>
2022-03-09 16:20:28 +0100

    Updated CryptoExchange.Net

--- Kucoin.Net.UnitTests/TestImplementations/TestHelpers.cs
+++ Kucoin.Net.UnitTests/TestImplementations/TestHelpers.cs
@@ -143,6 +143,7 @@ namespace Kucoin.Net.UnitTests.TestImplementations
             var request = new Mock<IRequest>();
             request.Setup(c => c.Uri).Returns(new Uri("http://www.test.com"));
             request.Setup(c => c.GetResponseAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(response.Object));
+            request.Setup(c => c.GetHeaders()).Returns(new Dictionary<string, IEnumerable<string>>());
 
             var factory = Mock.Get(client.RequestFactory);
             factory.Setup(c => c.Create(It.IsAny<HttpMethod>(), It.IsAny<Uri>(), It.IsAny<int>()))
