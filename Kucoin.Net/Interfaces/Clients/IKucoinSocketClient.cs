82a01697906acc9406444c1a430452573d67bf89
Jkorf <jankorf91@gmail.com>
2022-03-24 15:54:51 +0100

    Update IKucoinSocketClient.cs

--- Kucoin.Net/Interfaces/Clients/IKucoinSocketClient.cs
+++ Kucoin.Net/Interfaces/Clients/IKucoinSocketClient.cs
@@ -1,6 +1,7 @@
 ﻿using CryptoExchange.Net.Interfaces;
 using Kucoin.Net.Interfaces.Clients.FuturesApi;
 using Kucoin.Net.Interfaces.Clients.SpotApi;
+using Kucoin.Net.Objects;
 
 namespace Kucoin.Net.Interfaces.Clients
 {
@@ -17,5 +18,11 @@ namespace Kucoin.Net.Interfaces.Clients
         /// Futures streams
         /// </summary>
         IKucoinSocketClientFuturesStreams  FuturesStreams { get; }
+
+        /// <summary>
+        /// Set the API credentials for this client. All Api clients in this client will use the new credentials, regardless of earlier set options.
+        /// </summary>
+        /// <param name="credentials">The credentials to set</param>
+        void SetApiCredentials(KucoinApiCredentials credentials);
     }
 }
