07218734930515300e07e97e43a7ff34b11841e7
JKorf <jankorf91@gmail.com>
2024-06-19 17:00:34 +0200

    Update cc

--- Kucoin.Net/Clients/SpotApi/KucoinSocketClientSpotApi.cs
+++ Kucoin.Net/Clients/SpotApi/KucoinSocketClientSpotApi.cs
@@ -81,7 +81,7 @@ namespace Kucoin.Net.Clients.SpotApi
         }
 
         /// <inheritdoc />
-        protected override Query? GetAuthenticationRequest() => null;
+        protected override Query? GetAuthenticationRequest(SocketConnection connection) => null;
 
         /// <inheritdoc />
         public Task<CallResult<UpdateSubscription>> SubscribeToTickerUpdatesAsync(string symbol, Action<DataEvent<KucoinStreamTick>> onData, CancellationToken ct = default) => SubscribeToTickerUpdatesAsync(new[] { symbol }, onData, ct);
