337eb7e69fd6d3c2e0d3273cb52abc579839ad7d
Jkorf <jankorf91@gmail.com>
2025-08-22 14:52:05 +0200

    Added ClearUserClients method to user client provider

--- Kucoin.Net/Clients/KucoinUserClientProvider.cs
+++ Kucoin.Net/Clients/KucoinUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace Kucoin.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public IKucoinRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, KucoinEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))
