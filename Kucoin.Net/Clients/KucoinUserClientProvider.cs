0349a47124f67af84c78b05c431444473f5cee5c
Jan Korf <jankorf91@gmail.com>
2026-02-24 14:16:02 +0100

    CryptoExchange.Net update (#265)
    Updated CryptoExchange.Net to version 10.7.0
    Added additional Http settings to client options
    Updated Shared REST interfaces pagination logic
    Updated HttpClient registration, fixing issue of DNS changes not getting processed
    Fixed UserClientProvider using unconfigured HttpClient

--- Kucoin.Net/Clients/KucoinUserClientProvider.cs
+++ Kucoin.Net/Clients/KucoinUserClientProvider.cs
@@ -42,6 +42,7 @@ namespace Kucoin.Net.Clients
             IOptions<KucoinSocketOptions> socketOptions)
         {
             _httpClient = httpClient ?? new HttpClient();
+            _httpClient.Timeout = restOptions.Value.RequestTimeout;
             _loggerFactory = loggerFactory;
             _restOptions = restOptions;
             _socketOptions = socketOptions;
