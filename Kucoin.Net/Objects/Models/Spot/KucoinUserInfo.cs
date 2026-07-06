3bbe5593041cffec36b447256f995f7a8f2820d9
Jkorf <jankorf91@gmail.com>
2026-07-06 14:02:04 +0200

    Added OptionSubQuantity, MaxOptionSubQuantity to KucoinUserInfo model, updated quantity properties to int type

--- Kucoin.Net/Objects/Models/Spot/KucoinUserInfo.cs
+++ Kucoin.Net/Objects/Models/Spot/KucoinUserInfo.cs
@@ -15,46 +15,56 @@ namespace Kucoin.Net.Objects.Models.Spot
         /// ["<c>subQuantity</c>"] Max number of default open sub-accounts (according to level)
         /// </summary>
         [JsonPropertyName("subQuantity")]
-        public decimal SubQuantity { get; set; }
+        public int SubQuantity { get; set; }
         /// <summary>
         /// ["<c>maxDefaultSubQuantity</c>"] Max number of default open sub-accounts (according to level)
         /// </summary>
         [JsonPropertyName("maxDefaultSubQuantity")]
-        public decimal MaxDefaultSubQuantity { get; set; }
+        public int MaxDefaultSubQuantity { get; set; }
         /// <summary>
         /// ["<c>maxSubQuantity</c>"] Max number of sub-accounts = maxDefaultSubQuantity + maxSpotSubQuantity
         /// </summary>
         [JsonPropertyName("maxSubQuantity")]
-        public decimal MaxSubQuantity { get; set; }
+        public int MaxSubQuantity { get; set; }
         /// <summary>
         /// ["<c>spotSubQuantity</c>"] Number of sub-accounts with spot trading permissions enabled
         /// </summary>
         [JsonPropertyName("spotSubQuantity")]
-        public decimal SpotSubQuantity { get; set; }
+        public int SpotSubQuantity { get; set; }
+        /// <summary>
+        /// ["<c>optionSubQuantity</c>"] Number of sub-accounts with options trading permissions enabled
+        /// </summary>
+        [JsonPropertyName("optionSubQuantity")]
+        public int OptionSubQuantity { get; set; }
+        /// <summary>
+        /// ["<c>maxOptionSubQuantity</c>"] Max number of sub-accounts with options permissions
+        /// </summary>
+        [JsonPropertyName("maxOptionSubQuantity")]
+        public int MaxOptionSubQuantity { get; set; }
         /// <summary>
         /// ["<c>marginSubQuantity</c>"] Number of sub-accounts with margin trading permissions enabled
         /// </summary>
         [JsonPropertyName("marginSubQuantity")]
-        public decimal MarginSubQuantity { get; set; }
+        public int MarginSubQuantity { get; set; }
         /// <summary>
         /// ["<c>futuresSubQuantity</c>"] Number of sub-accounts with futures trading permissions enabled
         /// </summary>
         [JsonPropertyName("futuresSubQuantity")]
-        public decimal FuturesSubQuantity { get; set; }
+        public int FuturesSubQuantity { get; set; }
         /// <summary>
         /// ["<c>maxSpotSubQuantity</c>"] Max number of sub-accounts with additional Spot trading permissions
         /// </summary>
         [JsonPropertyName("maxSpotSubQuantity")]
-        public decimal MaxSpotSubQuantity { get; set; }
+        public int MaxSpotSubQuantity { get; set; }
         /// <summary>
         /// ["<c>maxMarginSubQuantity</c>"] Max number of sub-accounts with additional margin trading permissions
         /// </summary>
         [JsonPropertyName("maxMarginSubQuantity")]
-        public decimal MaxMarginSubQuantity { get; set; }
+        public int MaxMarginSubQuantity { get; set; }
         /// <summary>
         /// ["<c>maxFuturesSubQuantity</c>"] Max number of sub-accounts with additional futures trading permissions
         /// </summary>
         [JsonPropertyName("maxFuturesSubQuantity")]
-        public decimal MaxFuturesSubQuantity { get; set; }
+        public int MaxFuturesSubQuantity { get; set; }
     }
 }
