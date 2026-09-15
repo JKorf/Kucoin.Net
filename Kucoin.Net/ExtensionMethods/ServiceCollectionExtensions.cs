4862d4e3b3f6df71ce5365705112c12ead54bb68
Jkorf <jankorf91@gmail.com>
2026-09-15 15:11:38 +0200

    wip

--- Kucoin.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Kucoin.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -34,30 +34,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            var options = new KucoinOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-
-            try
-            {
-                configuration.Bind(options);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException("Invalid configuration provided", ex);
-            }
-
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            var restEnvName = options.Rest.Environment?.Name ?? options.Environment?.Name ?? KucoinEnvironment.Live.Name;
-            var socketEnvName = options.Socket.Environment?.Name ?? options.Environment?.Name ?? KucoinEnvironment.Live.Name;
-            options.Rest.Environment = KucoinEnvironment.GetEnvironmentByName(restEnvName) ?? options.Rest.Environment!;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = KucoinEnvironment.GetEnvironmentByName(socketEnvName) ?? options.Socket.Environment!;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = KucoinOptions.CreateFromConfiguration(configuration);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
@@ -75,19 +52,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             Action<KucoinOptions>? optionsDelegate = null)
         {
-            var options = new KucoinOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-            optionsDelegate?.Invoke(options);
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            options.Rest.Environment = options.Rest.Environment ?? options.Environment ?? KucoinEnvironment.Live;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = options.Socket.Environment ?? options.Environment ?? KucoinEnvironment.Live;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = KucoinOptions.Create(optionsDelegate);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
